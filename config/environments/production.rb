e8c83db52c737681c62f39b425163372473e5840
Ubuntu <ubuntu@ip-172-31-18-21.us-east-2.compute.internal>
2021-08-24 06:33:19 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,18 +2,17 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
   host = 'floating-spire-67266.herokuapp.com'
-   config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-     :port => 587,
-     :address => 'smtp.sendgrid.net',
-     :user_name => ENV['SENDGRID_USERNAME'],
-     :password => ENV['SENDGRID_PASSWORD'],
-     :domain => host,
-     :authentication => :plain,
-     :enable_starttls_auto => true
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
+    :authentication => :plain,
   }
   config.cache_classes = true
 
